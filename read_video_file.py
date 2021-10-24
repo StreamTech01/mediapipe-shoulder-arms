ee4373e5269627ee51be6063f55f1eb418875b01
Han Sheng <hanshengliang@outlook.com>
2021-10-24 16:12:01 +0800

    :tada: Init

---
+++ read_video_file.py
@@ -0,0 +1,64 @@
+import cv2
+import mediapipe as mp
+
+mp_drawing = mp.solutions.drawing_utils
+mp_drawing_styles = mp.solutions.drawing_styles
+mp_holistic = mp.solutions.holistic
+
+output_video = 'text.mp4'
+
+fourcc = cv2.VideoWriter_fourcc(*'MP4V')
+
+# Create a VideoCapture object and read from input file
+# If the input is the camera, pass 0 instead of the video file name
+cap = cv2.VideoCapture(0)
+out = cv2.VideoWriter(output_video,fourcc, 15, (int(cap.get(3)), int(cap.get(4))))
+
+# Check if camera opened successfully
+if cap.isOpened() == False:
+    print("Error opening video stream or file")
+
+# Read until video is completed
+with mp_holistic.Holistic(
+        min_detection_confidence=0.5,
+        min_tracking_confidence=0.5) as holistic:
+    while cap.isOpened():
+        success, image = cap.read()
+        if not success:
+            print("Ignoring empty camera frame.")
+            # If loading a video, use 'break' instead of 'continue'.
+            continue
+
+        # To improve performance, optionally mark the image as not writeable to
+        # pass by reference.
+        image.flags.writeable = False
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        results = holistic.process(image)
+
+        # Draw landmark annotation on the image.
+        image.flags.writeable = True
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+        # mp_drawing.draw_landmarks(
+        #     image,
+        #     results.face_landmarks,
+        #     mp_holistic.FACEMESH_CONTOURS,
+        #     landmark_drawing_spec=None,
+        #     connection_drawing_spec=mp_drawing_styles
+        #         .get_default_face_mesh_contours_style())
+        mp_drawing.draw_landmarks(
+            image,
+            results.pose_landmarks,
+            mp_holistic.POSE_CONNECTIONS,
+            landmark_drawing_spec=mp_drawing_styles
+                .get_default_pose_landmarks_style())
+        # Flip the image horizontally for a selfie-view display.
+        cv2.imshow('MediaPipe Holistic', cv2.flip(image, 1))
+        if cv2.waitKey(5) & 0xFF == 27:
+            break
+
+# When everything done, release the video capture object
+cap.release()
+# Save Video
+out.release()
+# Closes all the frames
+cv2.destroyAllWindows()
---
+++ real-time.py
@@ -0,0 +1,57 @@
+import os
+
+import cv2
+import mediapipe as mp
+
+mp_drawing = mp.solutions.drawing_utils
+mp_drawing_styles = mp.solutions.drawing_styles
+mp_hands = mp.solutions.hands
+
+fourcc = cv2.VideoWriter_fourcc(*'MP4V')
+
+# For webcam input:
+cap = cv2.VideoCapture(0)
+
+# Video Path here
+out = cv2.VideoWriter(f'outputs/v_1056_WRIST CIRCLES_VECTOR_V1.mp4', fourcc, 15, (int(cap.get(3)), int(cap.get(4))))
+with mp_hands.Hands(min_detection_confidence=0.5, min_tracking_confidence=0.5) as hands:
+    while (cap.isOpened()):
+        # Capture frame-by-frame
+        ret, frame = cap.read()
+        if ret == True:
+
+            image = cv2.cvtColor(cv2.flip(frame, 1), cv2.COLOR_BGR2RGB)
+            # To improve performance, optionally mark the image as not writeable to
+            # pass by reference.
+            image.flags.writeable = False
+            results = hands.process(image)
+
+            # Draw the hand annotations on the image.
+            image.flags.writeable = True
+            image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+            if results.multi_hand_landmarks:
+                for hand_landmarks in results.multi_hand_landmarks:
+                    mp_drawing.draw_landmarks(
+                        image,
+                        hand_landmarks,
+                        mp_hands.HAND_CONNECTIONS,
+                        mp_drawing_styles.get_default_hand_landmarks_style(),
+                        mp_drawing_styles.get_default_hand_connections_style())
+
+            # Display the resulting frame
+            cv2.imshow('Frame', image)
+            out.write(image)
+            # Press Q on keyboard to  exit
+            if cv2.waitKey(25) & 0xFF == ord('q'):
+                break
+
+        # Break the loop
+        else:
+            break
+
+    # When everything done, release the video capture object
+    cap.release()
+    # Save Video
+    out.release()
+    # Closes all the frames
+    cv2.destroyAllWindows()
